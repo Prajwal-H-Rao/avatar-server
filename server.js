d96b52a02e04a7f1d8db9a51dd070e58cabc81dd
Prajwal-H-Rao <prajwalhr130@gmail.com>
2025-03-14 04:35:01 +0000

    .

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post("/create-video", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
